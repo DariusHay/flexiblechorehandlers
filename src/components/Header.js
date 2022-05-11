37c6d8a3e5bf3b9596ca7dfdfa7eb1cb4dfceec8
Darius Hay <dariushay@gmail.com>
2022-05-11 00:04:00 -0400

    working on background

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,29 @@
 import React from 'react';
+import HomePic from './Images/home-pic.jpg'
 
 const Header = () => {
     return (
-        <div className="header-wrapper">
-           <div className="main-info">
-               <h1>
-                   Web Development and promotions
-               </h1>
-           </div>
+        <div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
+        <div class="carousel-inner">
+          <div class="carousel-item active">
+            <img src="..." class="d-block w-100" alt="..." />
+          </div>
+          <div class="carousel-item">
+            <img src={HomePic} class="d-block w-100" alt="..." />
+          </div>
+          <div class="carousel-item">
+            <img src="" class="d-block w-100" alt="..." />
+          </div>
         </div>
+        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
+          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+          <span class="visually-hidden">Previous</span>
+        </button>
+        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
+          <span class="carousel-control-next-icon" aria-hidden="true"></span>
+          <span class="visually-hidden">Next</span>
+        </button>
+      </div>
     )
 }
 
